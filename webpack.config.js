a2d72d52fce49bb2385706f3c942f05cc1403245
Vinicius Biondo <vkatatab@gmail.com>
2017-12-02 17:48:45 -0200

    Modules

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,22 @@ const config = {
     filename: 'bundle.js',
     publicPath: 'build/',
   },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx']
+  },
   module: {
     loaders: [
       { test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.less$/, loaders: ["style-loader", "css-loder", "less-loader"] },
     ]
   },
-    devServer: {
-      contentBase: __dirname,
-      historyApiFallback: true
+  devServer: {
+    contentBase: __dirname,
+    historyApiFallback: true
   }
 }
 
